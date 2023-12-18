3b22a7d57531b67d7c0789005ee4067bcb76f037
FannyieB <148464923+FannyieB@users.noreply.github.com>
2023-12-18 14:29:43 +0000

    commit14

--- addProduct.php
+++ addProduct.php
@@ -8,19 +8,7 @@ require_once ('Views/header.php');
 <form method="post" action="controller/addProductController.php">
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Nom du produit</label>
-        <!-- Ajout liste déroulante d'articles -->
-            <select class="form-control" id="product" name="product">
-                <option value="produit1">HIKOKI Perceuse visseuse à percussion 550 W</option>
-                <option value="produit2">DEWALT Perceuse visseuse 18V 5Ah</option>
-                <option value="produit3">HIKOKI Perceuse Visseuse 18V 5.0 Ah 55 Nm</option>
-                <option value="produit4">BOSCH Perceuse visseuse 12 V-GSR12V-15 housse</option>
-                <option value="produit5">BOSCH Perceuse visseuse 18V 2x4Ah GSR18V-55</option>
-                <option value="produit6">HIKOKI Perceuse à percussion 600W-DV16SSWAZ</option>
-                <option value="produit7">DEWALT Perceuse visseuse 18V 1 x 5Ah</option>
-                <option value="produit8">METABO Perceuse-visseuse 12V 2Ah PowerMaxx</option> 
-                <option value="produit9">METABO Perceuse visseuse 18V 4.0Ah BS18L</option>       
-             </select>
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="name">
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="name">
     </div>
     <div class="mb-3">
         <div class="form-floating">
